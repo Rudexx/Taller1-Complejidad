d488d23af18fdbe9eab743270bbddb196ccbf9ed
unknown <danielalvarez202@gmail.com>
2021-09-06 23:27:48 -0500

    Vista

--- src/co/edu/unbosque/view/VistaConsola.java
+++ src/co/edu/unbosque/view/VistaConsola.java
@@ -7,21 +7,34 @@ import javax.swing.JOptionPane;
 public class VistaConsola {
 
 
-
+	/**
+	 * Es el metodo contructor de la clase
+	 */
     public VistaConsola() {
 
     }
-
+    /**
+	 * Este metodo es para desplegar una lista con una serie de opciones predefinidas
+	 * @param mensaje Es el mensaje que aparecera explicandole al usuario la funcion de su eleccion
+	 * @param opciones Este arreglo contiene las opciones que contentra el mensaje
+	 * @return este metodo retornara la opcion elegida por el usuario
+	 */
     public String mostrarLista(String mensaje, Object[] opciones) {
     	Object o = JOptionPane.showInputDialog(null,mensaje, "Elegir",JOptionPane.QUESTION_MESSAGE,null,opciones, opciones[0]);
     	return  o.toString();
 
     }
-
+    /**
+	 * Este metodo es para mostrar un mensaje al usuario
+	 */
     public void mostrarInformacion( String mensaje) {
         JOptionPane.showMessageDialog(null, mensaje);
     }
-    
+    /**
+	 * Este metodo es para desplegar una lista con una serie de opciones predefinidas
+	 * @param mensaje Es el mensaje que aparecera explicandole al usuario la funcion de su eleccion
+	 * @return Este metodo retornara la opcion elegida por el usuario
+	 */
     public Integer mostrarOpcion(String mensaje) {
     	String [] options = {  "Automatico", "Manual"};
     	return JOptionPane.showOptionDialog(null,
@@ -33,7 +46,11 @@ public class VistaConsola {
     			options,  //the titles of buttons
     			options[0]); //default button title
     }
-
+    /**
+	 * Este metodo es para que el usuario pueda escribir el numero de datos
+	 * @param mensaje Es el mensaje que se le mostrara al usuario pidiendole el numero de datos
+	 * @return Se devolvera el numero de datos ingresado por el usuario.
+	 */
     public String leerDato(String mensaje) {
     	String opcion = "";
         opcion = JOptionPane.showInputDialog(mensaje);
